b11248eeed82f3f6758252a55c689426d4157515
Crytpo22h <crypto22h@gmail.com>
2022-10-04 02:37:05 +0100

    new cleaner way of doing things

--- src/popup.js
+++ src/popup.js
@@ -1,113 +1,144 @@
 'use strict';
 
-//Buttons
-const toggleBtnStr = "toggleBtn";
-const cancelBtnStr = "cancelBtn";
-const submitBtnStr = "submitBtn";
-
-//Inputs
-const usernameInputStr = "usernameInput";
-const descriptionInputStr = "descriptionInput";
-
 const getElem = (key) => document.getElementById(key);
 
-const getToggleBtn = () => getElem(toggleBtnStr);
-const getCancelBtn = () => getElem(cancelBtnStr);
-const getsubmitBtn = () => getElem(cancelBtnStr);
-
 const closePopup = () => {
-    window.close();
+  window.close();
 }
 
-const setHtmlText = (elem, text) => {
-    elem.innerText = text;
+const setElemText = (elem, text) => {
+  elem.innerText = text;
 }
 
 const setHtmlValue = (elem, text) => {
-    elem.value = text;
+  elem.value = text;
 }
 
-const getFromStorage = (key, returnBool = true) => {
-  if (returnBool) {
-    return localStorage[key] == 'true' ? true : false;
-  }
-
+const getStorage = (key) => {
   return localStorage[key];
 }
 
-const setToStorage = (key, val) => {
-    localStorage[key] = val;
+const toBool = (value) => {
+  if (value == null) return null; 
+  return value == 'true' ? true : false;
 }
 
-const updateStatus = (btn, key, trueTxt, falseTxt) => {
-  let status = getFromStorage(key);
-  let msg = status ? trueTxt : falseTxt;
-  setHtmlText(btn, msg);
+const setStorage = (key, val) => {
+  localStorage[key] = val;
+}
+
+const updateTxtField = (btn, key, trueTxt, falseTxt) => {
+  let storage = getStorage(key);
+  let status  = toBool(storage);
+  let msg     = status ? trueTxt : falseTxt;
+  setElemText(btn, msg);
 }
 
 const toggleBtnClick = () => {
+  let storage = getStorage(elements.onOff.key);
+
   // Load config
-  let addonStatus = getFromStorage(toggleBtnStr);
-  let btn = getToggleBtn();
+  let onOffBtn            = getElem(elements.onOff.id);
+  let onOffBtnConfig      = toBool(storage);
 
   // Save config
-  setToStorage(toggleBtnStr, !addonStatus);
-  updateStatus(btn, toggleBtnStr, 'Off', 'On');
+  setStorage(elements.onOff.key, !onOffBtnConfig);
+  updateTxtField(onOffBtn, elements.onOff.key, 'Off', 'On');
 }
 
 const saveBtnClick = () => {
-  let getUsernameInput = getElem(usernameInputStr).value;
-  let getDescriptionInput = getElem(descriptionInputStr).value;
+  let getUsernameInput    = getElem(elements.username.id).value;
+  let getDescriptionInput = getElem(elements.description.id).value;
   
-  setToStorage(usernameInputStr, getUsernameInput);
-  setToStorage(descriptionInputStr, getDescriptionInput);
+  setStorage(elements.username.key, getUsernameInput);
+  setStorage(elements.description.key, getDescriptionInput);
+}
+
+const elements = {
+  onOff: {
+    "element" : getElem("toggleBtn"),
+    "onclick" : toggleBtnClick,
+    "type"    : "button",
+    "id"      : "toggleBtn",
+    "key"     : "Enabled"
+  },
+  submit: {
+    "element" : getElem("submitBtn"),
+    "onclick" : saveBtnClick,
+    "type"    : "button",
+    "id"      : "submitBtn",
+    "key"     : null
+  },
+  cancel: {
+    "element" : getElem("cancelBtn"),
+    "onclick" : closePopup,
+    "type"    : "button",
+    "id"      : "cancelBtn",
+    "key"     : null
+  },
+  username: {
+    "element" : getElem("usernameInput"),
+    "onclick" : null,
+    "type"    : "textField",
+    "id"      : "usernameInput",
+    "key"     : "Username"
+  },
+  description: {
+    "element" : getElem("descriptionInput"),
+    "onclick" : null,
+    "type"    : "textField",
+    "id"      : "descriptionInput",
+    "key"     : "Description"
+  },
 }
 
 window.onload = () => {
-  // Load on/off status
-  let addonStatus = getFromStorage(toggleBtn);
+  let storage = getStorage(elements.onOff.key);
 
-  
+  // Load on/off status
+  let onOffBtn            = getElem(elements.onOff.id);
+  let onOffBtnConfig      = toBool(storage);
+
+  // Load Username
+  let usernameField       = getElem(elements.username.id);
+  let usernameConfig      = getStorage(elements.username.key);
+
+  // Load Description
+  let descriptionField    = getElem(elements.description.id);
+  let descriptionConfig   = getStorage(elements.description.key);
+
+  // Defaults
+  let defaultUsername     = "Anonymous";
+  let defaultDescription  = "No description";
+  let defaultOnOff        = false;
 
   // If null, set default values
   // Status
-  if (addonStatus == null) {
-    setToStorage(toggleBtnStr, false)
+  if (onOffBtnConfig == null) {
+    setStorage(elements.onOff.key, defaultOnOff)
+    onOffBtnConfig = defaultOnOff;
   }
   // Username
-  if (addonStatus == null) {
-    setToStorage(usernameInputStr, false)
+  if (usernameConfig == null) {
+    usernameConfig = defaultUsername;
+    setStorage(elements.username.key, defaultUsername)
   }
   // Description
-  if (addonStatus == null) {
-    setToStorage(descriptionInputStr, false)
+  if (descriptionConfig == null) {
+    descriptionConfig = defaultDescription;
+    setStorage(elements.description.key, defaultDescription)
   }
 
-
-  
-  let btn = getToggleBtn();
-  updateStatus(btn, toggleBtnStr, 'Off', 'On');
-
-  // Load Username
-  let usernameField     = getElem(usernameInputStr);
-  let usernameConfig    = getFromStorage(usernameInputStr, false);
-  if (usernameField.value != null) {
-    setHtmlValue(usernameField, usernameConfig);
-  }
-
-  // Load Description
-  let descriptionField  = getElem(descriptionInputStr);
-  let descriptionConfig = getFromStorage(descriptionInputStr, false);
-
-  if (descriptionField.value != null) {
-    setHtmlValue(descriptionField, descriptionConfig);
-  }
+  // Set values
+  updateTxtField(onOffBtn, elements.onOff.key, 'Off', 'On');
+  setHtmlValue(usernameField, usernameConfig);
+  setHtmlValue(descriptionField, descriptionConfig);
 }
 
 const listeners = {
-  "toggleBtn": toggleBtnClick,
-  "cancelBtn": closePopup,
-  "submitBtn": saveBtnClick
+  "toggleBtn": elements.onOff.onclick,
+  "cancelBtn": elements.cancel.onclick,
+  "submitBtn": elements.submit.onclick
 }
 
 for (let key in listeners) {
